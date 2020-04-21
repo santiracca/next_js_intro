94e7f33bfce1543c081cd59c3d372c4b44a2679d
santiracca <santiagoracca7@gmail.com>
2020-04-21 11:41:11 -0400

    Finish Basic Next.js App

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f0f38f3350da6c00e93b";
+/******/ 	var hotCurrentHash = "257187b98250af3a070c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
